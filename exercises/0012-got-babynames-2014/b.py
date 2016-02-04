eccf6711fe9c2b4305aefb80a942339347ee3d9b
Kaitlyn Landgraf <kaitlynl@stanford.edu>
2016-02-03 23:42:09 -0800

    babynames

--- exercises/0012-got-babynames-2014/b.py
+++ exercises/0012-got-babynames-2014/b.py
@@ -2,7 +2,7 @@ from os.path import join
 dir = '0012-got-babynames-2014/tempdata'
 f = join(dir, "ssa-babynames-nationwide-2014.txt")
 
-babies = 1
+babies = 0
 
 with open(f) as x:
 	for line in x:
